31f6d81ac41774d4ecf5047efafbe49e5e5d7730
Wojciech Niechoj <borock93@gmail.com>
2018-03-16 20:32:48 +0100

    Turret building

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen");
+            return;
+        }
+
+        //Build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation) as GameObject;
+    }
+}
